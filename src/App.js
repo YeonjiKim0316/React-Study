e3b6667aa3e9aa0437335f627e26bfc94e1e247d
yeonjikim0316 <kimyeonji3@gmail.com>
2021-07-01 17:09:58 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component{
   state = {
@@ -16,7 +16,7 @@ class App extends React.Component{
       }
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     // this.setState({movies:movies})
-    this.setState({movies, isLoading: false})
+    this.setState({ movies, isLoading: false })
   }
   componentDidMount(){ // 이 자리가 data를 fetch 하는 자리
     // setTimeout(() => {
@@ -28,12 +28,27 @@ class App extends React.Component{
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
-     <div>{isLoading ? "Loading" : movies.map(movie => {
-      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />);
-    })} </div>
-    </section>
+      <section className="container">
+     { isLoading ? ( <div className="loader"> 
+     <span className="loader__text">Loading...</span>
+     </div>
+     ) : (
+      <div className="movies">
+      {movies.map(movie => (
+      <Movie 
+      key={movie.id} 
+      id={movie.id} 
+      year={movie.year} 
+      title={movie.title} 
+      summary={movie.summary} 
+      poster={movie.medium_cover_image} 
+      genres={movie.genres} 
+      />
+      ))} </div> 
     )}
+    </section>
+    )
+  }
 }
 
 export default App;
